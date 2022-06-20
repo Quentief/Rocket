[fix] Add the derivative computations

controller/nox_bottle/pressure_rate.py
@@ -1,4 +1,5 @@
 import openmdao.api as om
+import numpy as np
 
 
 class PressureRate(om.ExplicitComponent):
@@ -20,7 +21,10 @@ class PressureRate(om.ExplicitComponent):
         self.add_output('p_dot', shape=(nn,), desc='Pressure change rate', units='Pa/s')
 
         # Derivative
-        self.declare_partials(of='*', wrt='*', method='fd')
+        ar = np.arange(nn)
+        self.declare_partials(of='*', wrt='*', rows=ar, cols=ar)
+        # self.declare_partials(of='*', wrt='*', method='fd')
+        # self.declare_partials(of='*', wrt='*', method='cs')
 
     def compute(self, inputs, outputs):
         p = inputs['p']
@@ -30,3 +34,16 @@ class PressureRate(om.ExplicitComponent):
         gamma = inputs['gamma']
 
         outputs['p_dot'] = gamma * p/(Vb - Vl) * Vl_dot
+
+    def compute_partials(self, inputs, partials):
+        p = inputs['p']
+        Vb = inputs['Vb']
+        Vl = inputs['Vl']
+        Vl_dot = inputs['Vl_dot']
+        gamma = inputs['gamma']
+
+        partials['p_dot', 'p'] = gamma/(Vb - Vl) * Vl_dot
+        partials['p_dot', 'Vb'] = gamma * -p/(Vb - Vl)**2 * Vl_dot
+        partials['p_dot', 'Vl'] = gamma * p/(Vb - Vl)**2 * Vl_dot
+        partials['p_dot', 'Vl_dot'] = gamma * p/(Vb - Vl)
+        partials['p_dot', 'gamma'] = p/(Vb - Vl) * Vl_dot

controller/nox_bottle/volume_flow_rate.py
@@ -25,7 +25,10 @@ class VolumeFlowRate(om.ExplicitComponent):
         self.add_output('Vl_dot', shape=(nn,), desc='Volume flow rate', units='m**3/s')
 
         # Derivative
-        self.declare_partials(of='*', wrt='*', method='fd')
+        ar = np.arange(nn)
+        self.declare_partials(of='*', wrt='*', rows=ar, cols=ar)
+        # self.declare_partials(of='*', wrt='*', method='fd')
+        # self.declare_partials(of='*', wrt='*', method='cs')
 
     def compute(self, inputs, outputs):
         p = inputs['p']
@@ -35,3 +38,19 @@ class VolumeFlowRate(om.ExplicitComponent):
         Aout = inputs['Aout']
 
         outputs['Vl_dot'] = Aout*np.sqrt(2/rhol*(p - pout - deltap))
+
+    def compute_partials(self, inputs, partials):
+        p = inputs['p']
+        pout = inputs['pout']
+        deltap = inputs['deltap']
+        rhol = inputs['rhol']
+        Aout = inputs['Aout']
+
+        pressure_diff = p - pout - deltap
+        dVldot_on_dp = Aout/np.sqrt(2*rhol*pressure_diff)
+
+        partials['Vl_dot', 'p'] = dVldot_on_dp
+        partials['Vl_dot', 'pout'] = -dVldot_on_dp
+        partials['Vl_dot', 'deltap'] = -dVldot_on_dp
+        partials['Vl_dot', 'rhol'] = -2*Aout*np.sqrt(pressure_diff)/rhol**2
+        partials['Vl_dot', 'Aout'] = 2/rhol*np.sqrt(pressure_diff)

model/nox_prop_calculator.py
@@ -20,22 +20,26 @@ class NOXProp():
     def find_from_t(self, temp: float):
         if temp >= self.temp_min and temp <= self.temp_max:
             return {
-                "psat": np.interp([temp], xp=self.nox_properties["t\n[K]"], fp=self.nox_properties["p\n[Pa]"]),
-                "rhol": np.interp([temp], xp=self.nox_properties["t\n[K]"], fp=self.nox_properties["rho l\n[kg/m3]"]),
-                "rhog": np.interp([temp], xp=self.nox_properties["t\n[K]"], fp=self.nox_properties["rho g\n[kg/m3]"])
+                "psat":  float(np.interp([temp], xp=self.nox_properties["t\n[K]"], fp=self.nox_properties["p\n[Pa]"])),
+                "rhol":  float(np.interp([temp], xp=self.nox_properties["t\n[K]"],
+                               fp=self.nox_properties["rho l\n[kg/m3]"])),
+                "rhog": float(np.interp([temp], xp=self.nox_properties["t\n[K]"],
+                                        fp=self.nox_properties["rho g\n[kg/m3]"]))
             }
         else:
-            return {"psat": np.nan, "rhol": np.nan, "rhog": np.nan}
+            return {"psat": float(np.nan), "rhol": float(np.nan), "rhog": float(np.nan)}
 
     def find_from_p(self, p: float):
         if p >= self.p_min and p <= self.p_max:
             return {
-                "psat": np.interp([p], xp=self.nox_properties["p\n[Pa]"], fp=self.nox_properties["t\n[K]"]),
-                "rhol": np.interp([p], xp=self.nox_properties["p\n[Pa]"], fp=self.nox_properties["rho l\n[kg/m3]"]),
-                "rhog": np.interp([p], xp=self.nox_properties["p\n[Pa]"], fp=self.nox_properties["rho g\n[kg/m3]"])
+                "Tsat": float(np.interp([p], xp=self.nox_properties["p\n[Pa]"], fp=self.nox_properties["t\n[K]"])),
+                "rhol": float(np.interp([p], xp=self.nox_properties["p\n[Pa]"],
+                                        fp=self.nox_properties["rho l\n[kg/m3]"])),
+                "rhog": float(np.interp([p], xp=self.nox_properties["p\n[Pa]"],
+                                        fp=self.nox_properties["rho g\n[kg/m3]"]))
             }
         else:
-            return {"psat": np.nan, "rhol": np.nan, "rhog": np.nan}
+            return {"Tsat": float(np.nan), "rhol": float(np.nan), "rhog": float(np.nan)}
 
     def find_Vl(self, m: float, psat: float, Vb: float, rhol: float, Tb: float):
         return (m - Vb*rhol)/(psat*self.MM/self.R/Tb - rhol)