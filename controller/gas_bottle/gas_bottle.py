[fix] Correct the partial derivatives + set them equals to 0 when pressure_diff equals to 0

controller/gas_bottle/gas_bottle.py
@@ -1,8 +1,8 @@
 import openmdao.api as om
 import numpy as np
+ 
 
-
-class NOXBottle(om.ExplicitComponent):
+class GasBottle(om.ExplicitComponent):
 
     def initialize(self):
         self.options.declare('num_nodes', types=int)
@@ -11,7 +11,7 @@ class NOXBottle(om.ExplicitComponent):
         nn = self.options['num_nodes']
 
         self.add_input('p', shape=(nn,), desc='Pressure inside the nox bottle', units='Pa')
-        self.add_input('pout', shape=(nn,), desc='Pressure outside the nox_bottle', units='Pa')
+        self.add_input('pout', shape=(nn,), desc='Pressure outside the gas_bottle', units='Pa')
         self.add_input('deltap', shape=(nn,), desc='Nox bottle pressure losses', units='Pa')
 
         self.add_input('Vb', shape=(nn,), desc='Bottle volume', units='m**3')
@@ -20,11 +20,14 @@ class NOXBottle(om.ExplicitComponent):
         self.add_input('gamma', shape=(nn,), desc='Heat capacity ratio')
 
         self.add_input('rhol', shape=(nn,), desc='Liquid density', units='kg/m**3')
-        self.add_input('Aout', shape=(nn,), desc='Output nox_bottle area', units='m**2')
+        self.add_input('Aout', shape=(nn,), desc='Output gas_bottle area', units='m**2')
 
         # Outputs
         self.add_output('Vl_dot', shape=(nn,), desc='Volume flow rate', units='m**3/s')
         self.add_output('p_dot', shape=(nn,), desc='Pressure change rate', units='Pa/s')
+
+        ar = np.arange(nn)
+        self.declare_partials(of='*', wrt='*', rows=ar, cols=ar)
 
         # self.declare_partials(of='*', wrt='*', method='cs')
         # self.declare_coloring(wrt=['*'], method='cs')
@@ -54,22 +57,33 @@ class NOXBottle(om.ExplicitComponent):
         gamma = inputs['gamma']
 
         pressure_diff = p - pout - deltap
-        Vl_dot = -Aout * np.sqrt(2/rhol * (p - pout - deltap))
 
-        partials['Vl_dot', 'p'] = -Aout/(2*rhol * pressure_diff)
-        partials['Vl_dot', 'pout'] = Aout/(2*rhol * pressure_diff)
-        partials['Vl_dot', 'deltap'] = Aout/(2*rhol * pressure_diff)
-        partials['Vl_dot', 'rhol'] = Aout*np.sqrt(pressure_diff/2)/rhol**(3/2)
-        partials['Vl_dot', 'Aout'] = -np.sqrt(2*pressure_diff/rhol)
+        with np.errstate(divide='ignore'):
+            dVldp = -Aout * np.sqrt(1 / (2 * rhol * pressure_diff))
+            dVldpout = Aout * np.sqrt(1 / (2 * rhol * pressure_diff))
+            dVlddeltap = Aout * np.sqrt(1 / (2 * rhol * pressure_diff))
+            dVldrhol = -Aout * np.sqrt(pressure_diff / (2 * rhol))
+            dVldAout = -np.sqrt(2 * pressure_diff / rhol)
+
+        for vector in [dVldp, dVldpout, dVlddeltap, dVldrhol, dVldAout]:
+            vector[np.isinf(vector)] = 0
+
+        partials['Vl_dot', 'p'] = dVldp
+        partials['Vl_dot', 'pout'] = dVldpout
+        partials['Vl_dot', 'deltap'] = dVlddeltap
+        partials['Vl_dot', 'rhol'] = dVldrhol
+        partials['Vl_dot', 'Aout'] = dVldAout
         partials['Vl_dot', 'Vb'] = 0
         partials['Vl_dot', 'Vl'] = 0
         partials['Vl_dot', 'gamma'] = 0
 
-        partials['p_dot', 'p'] = gamma/(Vb - Vl) * (Vl_dot - p * Aout/np.sqrt(2*rhol * pressure_diff))
-        partials['p_dot', 'pout'] = gamma/(Vb - Vl) * p * Aout/np.sqrt(2*rhol * pressure_diff)
-        partials['p_dot', 'deltap'] = gamma/(Vb - Vl) * p * Aout/np.sqrt(2*rhol * pressure_diff)
-        partials['p_dot', 'rhol'] = gamma/(Vb - Vl) * p * Aout * np.sqrt(pressure_diff/2)/rhol**(3/2)
-        partials['p_dot', 'Aout'] = -gamma/(Vb - Vl) * p * np.sqrt(2*pressure_diff/rhol)
+        Vl_dot = -Aout * np.sqrt(2 / rhol * (p - pout - deltap))
+
+        partials['p_dot', 'p'] = gamma * Vl_dot/(Vb - Vl) + gamma * p/(Vb - Vl) * dVldp
+        partials['p_dot', 'pout'] = gamma * p/(Vb - Vl) * dVldpout
+        partials['p_dot', 'deltap'] = gamma * p/(Vb - Vl) * dVlddeltap
+        partials['p_dot', 'rhol'] = gamma * p/(Vb - Vl) * dVldrhol
+        partials['p_dot', 'Aout'] = gamma * p/(Vb - Vl) * dVldAout
         partials['p_dot', 'Vb'] = -gamma * p * Vl_dot/(Vb - Vl)**2
-        partials['p_dot', 'Vl'] = -gamma * p * Vl_dot/(Vb - Vl)**2
-        partials['p_dot', 'gamma'] = p * Vl_dot/(Vb - Vl)**2
+        partials['p_dot', 'Vl'] = gamma * p * Vl_dot/(Vb - Vl)**2
+        partials['p_dot', 'gamma'] = p * Vl_dot/(Vb - Vl)